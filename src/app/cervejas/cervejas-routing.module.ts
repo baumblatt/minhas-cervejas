e0aa240a61ef5469521d8722a859c6a893521e1e
Bernado Baumblatt <baumblatt@gmail.com>
2019-10-03 02:42:39 -0300

    1.4 - Definição das Rotas

--- src/app/cervejas/cervejas-routing.module.ts
+++ src/app/cervejas/cervejas-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CervejaComponent} from './containers/cerveja/cerveja.component';
+import {CervejasComponent} from './containers/cervejas/cervejas.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: 'cervejas'},
+    {path: 'cervejas', component: CervejasComponent},
+    {path: 'cerveja/:id', component: CervejaComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+    imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class CervejasRoutingModule { }
