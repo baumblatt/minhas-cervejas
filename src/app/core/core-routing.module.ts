f62847ce1d8cb37197a3af378983d164e76e455d
Bernado Baumblatt <baumblatt@gmail.com>
2019-10-03 02:47:26 -0300

    1.4 - Definição das Rotas

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,8 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './containers/home/home.component';
+import {LayoutComponent} from './containers/layout/layout.component';
+import {LoginComponent} from './containers/login/login.component';
+import {SignupComponent} from './containers/signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: 'layout'},
+    {
+        path: 'layout', component: LayoutComponent, children: [
+            {path: '', pathMatch: 'full', redirectTo: 'home'},
+            {path: 'home', component: HomeComponent},
+            {path: 'login', component: LoginComponent},
+            {path: 'signup', component: SignupComponent},
+            {path: 'cervejas', loadChildren: () => import('../cervejas/cervejas.module').then(m => m.CervejasModule)},
+        ]
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
