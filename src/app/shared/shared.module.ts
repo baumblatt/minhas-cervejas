9589365f81955b9f91574d65709d11a228e681f2
Bernardo Baumblatt <baumblatt@gmaill.com>
2019-10-31 01:34:59 -0300

    2.2 - Adicionando Angular Flex-Layout.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MaterialModule} from './material/material.module';
 
@@ -8,11 +9,13 @@ import {MaterialModule} from './material/material.module';
     declarations: [],
     imports: [
         CommonModule,
+        FlexLayoutModule,
         MaterialModule,
         ReactiveFormsModule,
     ],
     exports: [
         CommonModule,
+        FlexLayoutModule,
         MaterialModule,
         ReactiveFormsModule,
     ]
